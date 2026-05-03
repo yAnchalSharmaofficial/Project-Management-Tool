1df3ce77922007a20963d1c48d9374ba54127b46
Anchal Sharma <anchalbasawa@gmail.com>
2026-05-03 21:54:31 +0530

    solved deployment error

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+   baseURL: `${process.env.REACT_APP_API_URL}/api/`,
 });
 
 API.interceptors.request.use((config) => {
